85e057d728a1b58cf30296ff6e6d3fb78ffe121a
joeytype <joeyflynn2001@gmail.com>
2023-04-19 01:31:17 +0100

    fixed form filling again

--- methods.py
+++ methods.py
@@ -15,37 +15,59 @@ def fill_form(complaint, link):
 
     # Find the input fields for the form and fill them with the provided input values
     first_name_input = driver.find_element(By.ID, "Textbox-1")
+    if not first_name_input.is_displayed():
+        driver.execute_script("arguments[0].scrollIntoView();", first_name_input)
     first_name_input.send_keys(complaint.first_name)
 
     surname_input = driver.find_element(By.ID, "Textbox-2")
+    if not surname_input.is_displayed():
+        driver.execute_script("arguments[0].scrollIntoView();", surname_input)
     surname_input.send_keys(complaint.surname)
 
     address_input = driver.find_element(By.ID, "Textbox-3")
+    if not address_input.is_displayed():
+        driver.execute_script("arguments[0].scrollIntoView();", address_input)
     address_input.send_keys(complaint.address)
 
     state_select = Select(driver.find_element(By.ID,"Dropdown-1"))
+    if not state_select.is_displayed():
+        driver.execute_script("arguments[0].scrollIntoView();", state_select)
     state_select.select_by_value(complaint.state_abbrev)
 
     city_input = driver.find_element(By.ID, "Textbox-4")
+    if not city_input.is_displayed():
+        driver.execute_script("arguments[0].scrollIntoView();", city_input)
     city_input.send_keys(complaint.city)
 
     zip_code_input = driver.find_element(By.ID,"Textbox-5")
+    if not zip_code_input.is_displayed():
+        driver.execute_script("arguments[0].scrollIntoView();", zip_code_input)
     zip_code_input.send_keys(complaint.zip_code)
 
     details_input = driver.find_element(By.ID,"Textarea-1")
+    if not details_input.is_displayed():
+        driver.execute_script("arguments[0].scrollIntoView();", details_input)
     details_input.send_keys(complaint.details)
 
     email_input = driver.find_element(By.ID,"Textbox-6")
+    if not email_input.is_displayed():
+        driver.execute_script("arguments[0].scrollIntoView();", email_input)
     email_input.send_keys(complaint.email)
 
     phone_input = driver.find_element(By.ID,"Textbox-7")
+    if not phone_input.is_displayed():
+        driver.execute_script("arguments[0].scrollIntoView();", phone_input)
     phone_input.send_keys(complaint.phone)
+
     # Find the submit button and click it to submit the form
     submit_button = driver.find_element(By.TAG_NAME, "button")
+    if not submit_button.is_displayed():
+        driver.execute_script("arguments[0].scrollIntoView();", submit_button)
     submit_button.click()
 
     # Close the browser window
     driver.quit()
+
 
 
 def pick_random_value(strings_list):
